8800abf6205c50f53ae385377892a196fa5673cd
mingruimingrui <mingruimingrui@hotmail.com>
2020-12-18 16:02:56 +0800

    Added class that caches a generator

---
+++ transformers_pretraining/cached_generator.py
@@ -0,0 +1,48 @@
+from types import GeneratorType
+from queue import Queue
+import threading
+from transformers_pretraining.queue_utils import enqueue, dequeue
+
+
+class CachedGenerator:
+    """A wrapper around a generator which caches the output.
+
+    Caching outputs allow the next entry to be available immediately on calling
+    __next__.
+    The problem with this wrapper is that it's not exactly efficient.
+    The expected use cases are mostly for generators with low generation rates
+    (< 10k/s) and even lower consumption rates (< 3k/s).
+    """
+
+    def __init__(self, generator: GeneratorType, cache_size: int = 2):
+        self.generator = generator
+        self.cache = Queue(maxsize=cache_size)
+
+        self.done_event = threading.Event()
+        self.shutdown_event = threading.Event()
+
+        def fill_cache():
+            for data in generator:
+                if self.shutdown_event.is_set():
+                    return
+                enqueue(self.shutdown_event, self.cache, data)
+            self.done_event.set()
+
+        self.worker = threading.Thread(target=fill_cache)
+        self.worker.setDaemon(True)
+        self.worker.start()
+
+    def close(self):
+        self.shutdown_event.set()
+        self.worker.join()
+
+    def __del__(self):
+        self.close()
+
+    def __next__(self):
+        if self.done_event.is_set() and self.cache.qsize() == 0:
+            raise StopIteration()
+        return dequeue(self.shutdown_event, self.cache)
+
+    def __iter__(self):
+        return self
